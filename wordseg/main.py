a2ad3f291ca74056c421f267a50b3b45cc8d468c
PaacMaan <ayoub.rmidi@gmail.com>
2019-07-06 16:56:45 +0200

    restructuring the main file

--- wordseg/main.py
+++ wordseg/main.py
@@ -1,12 +1,19 @@
 from wordseg import segment_token
 
 
-if __name__ == '__main__':
-
+def main():
 	# set a test token
 	token = "soitmoinscompliqué"
 
+	# apply segmentation fucntion on the given token
 	result = segment_token(token)
 
+	# show results
 	print("raw token is {}".format(token))
 	print("processed token is {}".format(result))
+
+
+
+if __name__ == '__main__':
+	main()
+	
