58ad35b65d37dfd3a0441bba97dafd6142506d73
PaacMaan <ayoub.rmidi@gmail.com>
2019-07-29 17:08:29 +0200

    restructure setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(
-    name='semantic_word_segmentation',
-    version='0.1',
-    packages=['semantic_word_segmentation'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
-)
+setup(name='fr_word_segment',
+      version='0.1',
+      description='A package that split mispelled words semantically',
+      url='https://github.com/PaacMaan/semantic-word-segmentation',
+      author='Ayoub RMIDI',
+      author_email='ayoub.rmidi@gmail.com',
+      license='MIT',
+      packages=['fr_word_segment'],
+      install_requires=[
+          'spacy',
+      ],
+      zip_safe=False)
